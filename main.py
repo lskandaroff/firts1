61b553a784928faa0e76693d21c8c26f8ba86b62
Doston <dostoniskandarov0204@gmail.com>
2024-11-07 21:45:27 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+# Vazifa______________________________________________-
+
+# 1
+
+# mahsulot = ["olma", "banan", "non", "sut"]
+# print(hex(id(mahsulot)))
+# mahsulot.append(["tuxum", "yog"])
+# print(hex(id(mahsulot)))
+
+# 2
+
+# number = [3, 7, 2, 9, 1]
+# print(max(number))
+# print(min(number))
+
+# 3
+# while True:
+#
+#     user = input("ismni kiriting: ")
+#     customers = {"Ali": "3 ta non", "Aziza": "2 ta olma", "Jasur": "1 ta kola", "Dilnoza": "5 ta banan"}
+#     for i in customers:
+#         if i == user:
+#             print(customers[i])
+#
+#     if user == "stop".lower():
+#         break
+
+# 4
+# juft_sonlar = []
+# toq_sonlar = []
+#
+# for i in range(1, 101):
+#     if i % 2 != 0:
+#         toq_sonlar.append(i)
+#     else:
+#         juft_sonlar.append(i)
+#
+# print(juft_sonlar)
+# print(toq_sonlar)
+
+# 5
+
+# numbers = [5, 2, 9, 1, 5, 6]
+#
+# numbers.sort()
+#
+# a = 0
+# for i in numbers:
+#     a += i
+#
+# a = a / len(numbers)
+# print(a)
+
+
+
+
+
